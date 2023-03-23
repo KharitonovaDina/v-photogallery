29c308a9436f973617eff84958e075092123b1b0
KharitonovaDina <d.i.haritonova@yandex.ru>
2023-03-23 15:23:53 +0700

    add new components: UiPhotoDialog, UiPhotoForm

---
+++ src/components/Photo/UiPhotoDialog.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-dialog
+    v-model="value"
+    max-width="600"
+  >
+    <v-card>
+      <v-card-title>{{ photo.title }}</v-card-title>
+      <v-card-text>
+        <v-img
+          :src="photo.url"
+        />
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'UiPhotoDialog',
+  props: {
+    photo: {
+      type: Object,
+      required: true,
+    },
+    value: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  created() {
+    this.dialogVisible = this.value;
+  },
+  data: () => ({
+    dialogVisible: false,
+  }),
+  watch: {
+    value(newValue) {
+      this.dialogVisible = newValue;
+    },
+    dialogVisible(newValue) {
+      this.$emit('input', newValue);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/PhotosPage.vue
+++ src/views/PhotosPage.vue
@@ -1,25 +1,34 @@
 <template>
   <v-container>
+    <UiPhotoForm @addPhoto="addNewPhoto"/>
     <v-row>
       <UiPhoto
         v-for="item in photos"
         :key="item.id"
         :photo="item"
+        @openPhoto="openCard"
       />
     </v-row>
+    <UiPhotoDialog :photo="currentPhoto" v-model="dialogVisible"/>
   </v-container>
 </template>
 
 <script>
 import UiPhoto from '@/components/Photo/UiPhoto.vue';
+import UiPhotoForm from '@/components/Photo/UiPhotoForm.vue';
+import UiPhotoDialog from '@/components/Photo/UiPhotoDialog.vue';
 
 export default {
   components: {
+    UiPhotoDialog,
     UiPhoto,
+    UiPhotoForm,
   },
 
   data: () => ({
     photos: [],
+    currentPhoto: {},
+    dialogVisible: false,
   }),
 
   mounted() {
@@ -32,6 +41,14 @@ export default {
         .then((response) => {
           this.photos = response.data;
         });
+    },
+
+    addNewPhoto(photo) {
+      this.photos.push(photo);
+    },
+    openCard(photo) {
+      this.currentPhoto = photo;
+      this.dialogVisible = true;
     },
   },
 };
